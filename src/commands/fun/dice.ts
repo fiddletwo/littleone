c91dbd7c1762f490dc5a2b6656bf24239bc03e2b
fiddletwo <328152338+fiddletwo@users.noreply.github.com>
2026-09-12 11:03:57 -0400

    feat: roll multiple dice

--- src/commands/fun/dice.ts
+++ src/commands/fun/dice.ts
@@ -2,9 +2,16 @@ import { Command } from "../../utils/commands";
 
 const command: Command = {
     name: "dice",
-    run: async (respond) => {
-        const random = Math.floor(Math.random() * 5 + 1);
-        await respond(`You rolled a ${random}!`);
+    run: async (respond, args) => {
+        const count = args[0] ? parseInt(args[0]) : 1;
+
+        const diceRolls: number[] = [];
+        for (let i = 0; i < count; i++) {
+            const random = Math.floor(Math.random() * 5 + 1);
+            diceRolls.push(random);
+        }
+
+        await respond(`You rolled ${diceRolls.join(", ")}!`);
     },
 };
 
